36ad1442d7861afa51d455b56b9c21b946771b2c
kc <choudhary.kus@northeastern.edu>
2022-11-18 15:22:32 -0500

    commit

---
+++ test.js
@@ -0,0 +1,9 @@
+let data={
+    "fromAddress":"no-reply@demo.choudharykusha.me",
+    "toAddress":"kushach@@gmail.com",
+    "userName":"kusha",
+    "link":"http://localhost:4000/verifyEmail?",
+    "domain":"demo.choudharykusha.me"
+  }
+  
+  console.log(JSON.stringify(data));
